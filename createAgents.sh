769940ebba1175122f2e7f045194d25294982f7a
Nemo <viole_day@live.com>
2015-04-08 12:18:05 +0800

    添加sh

--- createAgents.sh
+++ createAgents.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 ps -ef |grep "/home/ubuntu/.nvm/versions/node/v0.12.1/bin/node app client"|awk '{print $2}'|xargs kill -9
-for((i=1;i<=29;i++));do
-/home/ubuntu/.nvm/versions/node/v0.12.1/bin/node app client &
-done
+for((i=1;i<=30;i++));do /home/ubuntu/.nvm/versions/node/v0.12.1/bin/node app client & done
